288b4a0c9659cbee034f8d05ae6a6c83e5661628
qnddjKJH.dev <qnddjKJH.dev@gmail.com>
2021-03-18 17:26:27 +0900

    21.03.18. 진도

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// React.Component 는 return 을 가지고 있지 않다
+// render Method 가 존재
+// state 를 직접적으로 변경해서는 안된다.
+// this.state 에 너무 의존하지 말자 current => () 처럼 function 형식을 지원한다.
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movioe: []
+  };
+
+  // 이론적으로 componentDidMount 에서 data를 fetch 한다
+  componentDidMount() {
+    setTimeout( () => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
+ 
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        { isLoading ? "Loading..." : "We are ready" }
+      </div>
+    );
+  };
 }
 
 export default App;
