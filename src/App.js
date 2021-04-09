f2cffebe70895ef180ff37a8f5c91c939ac7cca4
qnddjKJH.dev <qnddjKJH.dev@gmail.com>
2021-04-09 21:11:02 +0900

    강의 끝
     -- 뒤에 더 있는데 gh-page 를 사용한 배포방법임

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 import './App.css';
 
 // React.Component 는 return 을 가지고 있지 않다
@@ -8,24 +10,47 @@ import './App.css';
 class App extends React.Component {
   state = {
     isLoading: true,
-    movioe: []
+    movies: []
   };
+  getMovies = async () => {
+    const { 
+      data: { 
+        data: { movies } 
+      } 
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies: movies, isLoading: false });
+  }
 
   // 이론적으로 componentDidMount 에서 data를 fetch 한다
   componentDidMount() {
-    setTimeout( () => {
-      this.setState({ isLoading: false });
-    }, 3000);
+    this.getMovies();
   }
  
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        { isLoading ? "Loading..." : "We are ready" }
-      </div>
+      <section className="container">
+        { isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div> ) 
+          : ( 
+            <div className="movies">
+              {movies.map(movie => (      
+              < Movie 
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image} 
+                genres={movie.genres}
+              />
+              ))}
+            </div>
+          )}
+      </section>
     );
-  };
+  }
 }
 
 export default App;
